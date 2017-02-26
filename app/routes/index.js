316c19370d1469d56116ae2d5817b29520a0d95c
amzac <amaury.bauzac@gmail.com>
2017-02-26 17:06:25 +0100

    step model hook

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     beforeModel(){
+        // replace with is different from transitionTo function.
+        // transitionTo will add the route to the browser's history
         this.replaceWith("rentals");
     }
 });
